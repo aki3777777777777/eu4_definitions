672fb40a3ecb7e42fb6adeea63aa7b8282b4adbb
aki3777777777777 <niwatori382@gmail.com>
2023-02-26 13:00:11 +0900

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ path = 'data/definition.csv'
 all_provinces = ""
 sea_provinces = ""
 lake_provinces = ""
+land_provinces = ""
 text = ""
 
 def writef(path, con):
@@ -15,8 +16,8 @@ def writef(path, con):
 
 df = pd.read_csv(path,sep=";",header=None)
 
-for item in df.loc[:].values:
-    all_provinces += str(item[0]) + " "
+for item in df[df[5] == 'Land'][0].values:
+    land_provinces += str(item) + " "
 for item in df[df[5] == 'Sea'][0].values:
     sea_provinces += str(item) + " "
 for item in (df[df[5] == 'Lake'][0].values):
@@ -25,9 +26,9 @@ for item in (df[df[5] == 'Lake'][0].values):
 os.makedirs('out/map', exist_ok=True)
 
 writef('out/map/default.map', "width = 5632 \nheight = 2048 \nmax_provinces =" + str(len(df) + 1) + "\nsea_starts = {" + sea_provinces +"} \nonly_used_for_random = {} \nlakes = {" + lake_provinces + "} \nforce_coastal = {} \ntree = {}" + '\ndefinitions = "definition.csv" \nprovinces = "provinces.bmp" \npositions = "positions.txt" \nterrain = "terrain.bmp" \nrivers = "rivers.bmp" \nterrain_definition = "terrain.txt" \nheightmap = "heightmap.bmp" \ntree_definition = "trees.bmp" \ncontinent = "continent.txt" \nadjacencies = "adjacencies.csv" \nclimate = "climate.txt" \nregion = "region.txt" \nsuperregion = "superregion.txt" \narea = "area.txt" \nprovincegroup = "provincegroup.txt" \nambient_object = "ambient_object.txt" \nseasons = "seasons.txt" \ntrade_winds = "trade_winds.txt"')
-writef('out/map/area.txt', "black_sea_area = {" + all_provinces + "}") #一時的に全プロビを黒海地域に入れる
-writef('out/map/climate.txt', "tropical = {" + all_provinces + "}") #一時的に全プロビを熱帯に
-writef('out/map/continent.txt', "europe = {" + all_provinces + "}") #一時的に全プロビをヨーロッパに
+writef('out/map/area.txt', "black_sea_area = {" + land_provinces + "}") #一時的に全プロビを黒海地域に入れる
+writef('out/map/climate.txt', "tropical = {" + land_provinces + "}") #一時的に全プロビを熱帯に
+writef('out/map/continent.txt', "europe = {" + land_provinces + "}") #一時的に全プロビをヨーロッパに
 writef('out/map/region.txt', "france_region = {black_sea_area}") #一時的に地域をフランスに
 writef('out/map/superregion.txt', "india_superregion = {france_region}") #一時的にｚ全リージョンをインドに
 
@@ -39,4 +40,7 @@ os.makedirs('out/history/provinces', exist_ok=True)
 
 for item in df.loc[:].values:
     path2 = 'out/history/provinces/' + str(item[0]) + '-' + str(item[4]) + '.txt'
-    writef(path2, 'add_core = SWE\nowner = SWE\ncontroller = SWE\nculture = swedish\nreligion = catholic\nhre = no\nbase_tax = 1\nbase_production = 1\ntrade_goods = grain\nbase_manpower = 1\ncapital = "cpital"is_city = yes\ndiscovered_by = western\nextra_cost = 16')
+    if item[5] == "Land":
+        writef(path2, 'add_core = SWE\nowner = SWE\ncontroller = SWE\nculture = swedish\nreligion = catholic\nhre = no\nbase_tax = 1\nbase_production = 1\ntrade_goods = grain\nbase_manpower = 1\ncapital = "cpital"\nis_city = yes\ndiscovered_by = western\nextra_cost = 16')
+    else:
+        writef(path2, 'discovered_by = western')
