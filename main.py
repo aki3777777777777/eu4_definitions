cab617ddd71f7cd8629f4ad442391f3d5e84815f
aki3777777777777 <niwatori382@gmail.com>
2023-02-26 16:08:09 +0900

    aaa

--- main.py
+++ main.py
@@ -27,10 +27,12 @@ os.makedirs('out/map', exist_ok=True)
 
 writef('out/map/default.map', "width = 5632 \nheight = 2048 \nmax_provinces =" + str(len(df) + 1) + "\nsea_starts = {" + sea_provinces +"} \nonly_used_for_random = {} \nlakes = {" + lake_provinces + "} \nforce_coastal = {} \ntree = {}" + '\ndefinitions = "definition.csv" \nprovinces = "provinces.bmp" \npositions = "positions.txt" \nterrain = "terrain.bmp" \nrivers = "rivers.bmp" \nterrain_definition = "terrain.txt" \nheightmap = "heightmap.bmp" \ntree_definition = "trees.bmp" \ncontinent = "continent.txt" \nadjacencies = "adjacencies.csv" \nclimate = "climate.txt" \nregion = "region.txt" \nsuperregion = "superregion.txt" \narea = "area.txt" \nprovincegroup = "provincegroup.txt" \nambient_object = "ambient_object.txt" \nseasons = "seasons.txt" \ntrade_winds = "trade_winds.txt"')
 writef('out/map/area.txt', "black_sea_area = {" + land_provinces + "}") #一時的に全プロビを黒海地域に入れる
+writef('out/map/area.txt', "sea_area = {" + sea_provinces + "}") #海プロビ用地域
 writef('out/map/climate.txt', "tropical = {" + land_provinces + "}") #一時的に全プロビを熱帯に
 writef('out/map/continent.txt', "europe = {" + land_provinces + "}") #一時的に全プロビをヨーロッパに
 writef('out/map/region.txt', "france_region = {black_sea_area}") #一時的に地域をフランスに
-writef('out/map/superregion.txt', "india_superregion = {france_region}") #一時的にｚ全リージョンをインドに
+writef('out/map/region.txt', "japan_region = {sea_area}") #海プロビンス用リージョン
+writef('out/map/superregion.txt', "india_superregion = {france_region}") #一時的に全リージョンをインドに
 
 for item in df.loc[:,[0]].values:
     text += str(item[0]) + " = {position={0.000 0.000 0.000 0.000 0.000 0.000 0.000 0.000 0.000 0.000 0.000 0.000 0.000 0.000} \nrotation={0.000 0.000 0.000 0.000 0.000 0.000 0.000} \nheight={0.000 0.000 0.000 0.000 0.000 0.000 0.000}}\n"
